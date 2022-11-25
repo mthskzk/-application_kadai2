e379aa02761426ecb2d5cd87e0923cb588d687b0
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-11-25 12:10:26 +0000

    [Finish]応用課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -32,15 +34,25 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
+
+  def is_matching_login_user
+    book=Book.find(params[:id])
+    user_id=book.user_id
+    login_user_id=current_user.id
+    if(user_id != login_user_id)
+      redirect_to books_path
+    end
+  end
+
 end
