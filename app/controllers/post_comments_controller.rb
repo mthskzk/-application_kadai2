9af70c1487adea73f39abe9e9bf2feb268be054d
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-12-07 11:34:21 +0000

    [Finish]応用課題6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,17 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book=Book.find(params[:book_id])
-    comment=current_user.post_comments.new(post_comment_params)
-    comment.book_id=book.id
-    comment.save
-    redirect_to book_path(book)
+    @book=Book.find(params[:book_id])
+    @post_comment=current_user.post_comments.new(post_comment_params)
+    @post_comment.book_id=@book.id
+    @post_comment.save
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book=Book.find(params[:book_id])
+    @post_comment=PostComment.find(params[:id])
+    @post_comment.destroy
+    @post_comment=PostComment.new
   end
 
   private
